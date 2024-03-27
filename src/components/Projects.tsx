f305e6c56a904aff1778bd624960ff2017725d6f
okaynils <nils.fahrni@students.fhnw.ch>
2024-03-27 12:03:58 +0100

    added projects list

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,50 +1,56 @@
 import React, { useState, useEffect } from "react";
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import Podcast from "../viewmodels/Podcast";
-import GET_PODCASTS_QUERY from "../api/queries.taddy";
 
-const Projects = () => {
-
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [podcasts, setPodcasts] = useState<Podcast[]>([]);
-
-    useEffect(() => {
-        const client = new ApolloClient({
-            uri: 'https://api.taddy.org/',
-            cache: new InMemoryCache(),
-            headers: {
-                "X-API-Key": process.env.TADDY_API_KEY!.toString(),
-                "X-USER-ID": process.env.TADDY_USERID!.toString()
-            },
-        });
-
-        client.query({
-            query: GET_PODCASTS_QUERY
-        })
-        .then((result) => setPodcasts(Object.values(result["data"])))
-        .catch((error) => setError(error));
-        setIsLoaded(true);
-    }, []);
-    
-    if (error) {
-        return error
-    } else if (!isLoaded) {
-        return <div className="loading">Loading favorite podcasts...</div>;
-    } else {
-        return (
-            <div>
-                <div className="cover-container">
-                    {podcasts.map(podcast => (
-                        <div className="cover-item">
-                            <img className="cover-image" src={podcast["imageUrl"]} alt="Cover 1" />
-                            <p className="podcastName">{podcast["name"]}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        );
-    }
+interface Repository {
+    id: number;
+    name: string;
+    html_url: string;
 }
+
+const Projects: React.FC = () => {
+    // Use the Repository[] type for the state
+    const [repos, setRepos] = useState<Repository[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+  
+    useEffect(() => {
+      const fetchRepos = async () => {
+        setIsLoading(true);
+        try {
+          const response = await fetch('https://api.github.com/users/okaynils/repos?type=sources&per_page=4&sort=pushed');
+          if (!response.ok) {
+            throw new Error('Failed to fetch');
+          }
+          const data: Repository[] = await response.json();
+
+          setRepos(data);
+        } catch (error) {
+          console.error('Error fetching repositories:', error);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+  
+      fetchRepos();
+    }, []);
+  
+    return (
+      <div>
+        {isLoading ? (
+          <p>Loading repositories...</p>
+        ) : (
+          <ul>
+            {repos.map(repo => (
+              <li key={repo.id} className="artist-li">
+                <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                    <div className="artist-div">
+                        {repo.name}
+                    </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  };
 
 export default Projects;
